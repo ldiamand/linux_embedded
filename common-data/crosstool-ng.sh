fc5192f1a02ec862239f7ffef3144da0140355a5
Luciano Daniel Diamand <lucianodiamand@yahoo.com>
2018-11-20 07:05:28 -0300

    Agregado de la verificación del toolchain

--- common-data/crosstool-ng.sh
+++ common-data/crosstool-ng.sh
@@ -1,11 +1,33 @@
 #!/bin/sh
 
+# Descargamos crosstool-ng
 git clone https://github.com/crosstool-ng/crosstool-ng.git
 cd crosstool-ng/
+
+# Construimos crosstool-ng
 ./bootstrap
 ./configure --prefix=`pwd`
 make && make install
 
+# Compiamos la configuración pre-armada
 cp ../data/crosstool-ng.config .config
+
+# Generamos el juego de herramientas
 ./ct-ng build
+
+# Limpiamos el entorno
+./ct-ng clean
+
+cd ../data
+
+# Probamos el juego de herramientas
+${CROSS_COMPILE}gcc --static hello.c -o hello_static
+
+# Probamos el Makefile
+make
+
+cd ..
+
+# Verificamos los generados
+./verify-toolchain.sh
 
