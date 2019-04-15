c3afbec6a715f0028cb6bf69de5c0706438eed84
Luciano Daniel Diamand <lucianodiamand@gmail.com>
2019-04-15 18:33:30 -0300

    Corrección de los scripts

--- scripts/common/kernel.sh
+++ scripts/common/kernel.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
+# Vamos al directorio de trabajo
+cd ${PROJECT_ROOT}/kernel
+
 # Descargamos el kernel
 git clone -b v5.0.7 --depth 1 \
     git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
 cd linux-stable
 
 # Compiamos la configuración pre-armada
-cp ../data/linux.config .config
+cp ${PROJECT_ROOT}/kernel/data/linux.config .config
 
 # Generamos u-boot
 make -j 8
 
 # Copiamos el kernel generado a la carpeta data
-cp ${KERNEL_PATH}/${KERNEL_IMAGE} ../data
+cp ${KERNEL_PATH}/${KERNEL_IMAGE} ${PROJECT_ROOT}/kernel/data
 
 # Copiamos el dtb generado a la carpeta data
-cp ${DTB_PATH}/${DTB_IMAGE} ../data
+cp ${DTB_PATH}/${DTB_IMAGE} ${PROJECT_ROOT}/kernel/data
 
