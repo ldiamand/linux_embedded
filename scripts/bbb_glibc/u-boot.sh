187e63ba2850fb4a533f23db8bae51a337c74ab0
Luciano Daniel Diamand <lucianodiamand@gmail.com>
2019-04-16 20:49:06 -0300

    Ultimas actualizaciones

---
+++ scripts/bbb_glibc/u-boot.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+export WORK=${PROJECT_ROOT}/bootloader
+export DATA=${WORK}/data
+
+cd ${WORK}
+
+# Descargamos u-boot
+git clone git://git.denx.de/u-boot.git
+cd u-boot
+git checkout v2019.04
+
+# Compilamos la configuración pre-armada
+cp ${DATA}/u-boot.config .config
+
+# Generamos u-boot
+make -j8
+
+# Copiamos el u-boot generado a la carpeta data
+cp ${UBOOT_PATH}/${MLO_IMG} ${DATA}
+cp ${UBOOT_PATH}/${UBOOT_IMG} ${DATA}
+
+# Corregir el compilador para herramientas nativas
+# Ver porque pide el /etc/fw_env.config.
+# Compilamos las herramientas de entorno
+#make -j8 envtools
+
+# y lo instalamos en la carpeta bin
+#ln -s tools/env/fw_printenv ${PROJECT_ROOT}/bin
+#ln -s tools/env/fw_printenv ${PROJECT_ROOT}/bin/fw_setenv
+
--- scripts/bbb_glibc/verify-toolchain.sh
+++ scripts/bbb_glibc/verify-toolchain.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+export DATA=${PROJECT_ROOT}/build-tools/data
+
 # TODO actualiza
 # Verificamos la salida de gcc
-file data/hello_static | grep -q 'ELF 32-bit LSB executable, ARM, EABI5 version 1 (SYSV), statically linked' && \
+file ${DATA}/hello_static | grep -q 'ELF 32-bit LSB executable, ARM, EABI5 version 1 (SYSV), statically linked' && \
     echo 'Compilación con bibliotecas estáticas... OK'
 
 # TODO actualizar
 # Verificamos la salida de make
-file data/hello | grep -q 'ELF 32-bit LSB executable, ARM, EABI5 version 1 (SYSV), dynamically linked, interpreter /lib/ld-linux-armhf.so.3' && \
+file ${DATA}/hello | grep -q 'ELF 32-bit LSB executable, ARM, EABI5 version 1 (SYSV), dynamically linked, interpreter /lib/ld-linux-armhf.so.3' && \
     echo 'Compilación con bibliotecas dinamicas... OK'
+
 
