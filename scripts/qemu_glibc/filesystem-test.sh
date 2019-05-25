c29087a73f6ef56208054b7f2779eea2b69c7870
Luciano Daniel Diamand <lucianodiamand@gmail.com>
2019-05-25 12:14:32 -0300

    Corrección en la ejecución de truncate

--- scripts/qemu_glibc/filesystem-test.sh
+++ scripts/qemu_glibc/filesystem-test.sh
@@ -7,10 +7,13 @@ export DTB_IMAGE=${DATA}/vexpress-v2p-ca9.dtb
 export ROOT_FILE_SYSTEM=${PROJECT_ROOT}/images/rootfs.sqfs
 
 mksquashfs rootfs/ images/rootfs.sqfs -noappend
-dd if=images/rootfs.sqfs of=images/sd.img bs=1M conv=notrunc status=progress
+truncate -s %256k images/rootfs.sqfs
+
+#dd if=images/rootfs.sqfs of=images/sd.img bs=1M conv=notrunc status=progress
 
 QEMU_AUDIO_DRV=none qemu-system-arm -M vexpress-a9 -m 128M -nographic \
-  -kernel ${KERNEL_IMAGE} -dtb ${DTB_IMAGE} -append "console=ttyAMA0 root=/dev/sda rootfstype=squashfs" -hda images/hdb.img # \
-#  -drive file=images/sd.img,if=sd,format=raw
+  -kernel ${KERNEL_IMAGE} -dtb ${DTB_IMAGE} \
+  -drive file=images/rootfs.sqfs,if=sd,format=raw \
+  -append "console=ttyAMA0 root=/dev/mmcblk0 rootfstype=squashfs"
 
 #qemu-system-x86_64 -kernel bzImage -initrd initrd.img-3.11 -append "root=/dev/ram rdinit=/sbin/init"
