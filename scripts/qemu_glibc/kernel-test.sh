b61a48ed579bc9ef7a6ff3edaad923cba26226f0
Luciano Daniel Diamand <ldiaman0@rosario.gov.ar>
2019-09-02 13:29:42 -0300

    Ultimos cambios

--- scripts/qemu_glibc/kernel-test.sh
+++ scripts/qemu_glibc/kernel-test.sh
@@ -3,7 +3,7 @@
 # No va otra variable definida en el script principal
 export DATA=${PROJECT_ROOT}/kernel/data
 export KERNEL_IMAGE=${DATA}/zImage
-export DTB_IMAGE=${DATA}/vexpress-v2p-ca9.dtb
+export DTB_IMAGE=${DATA}/nova.dtb
 
 QEMU_AUDIO_DRV=none qemu-system-arm -M ${QEMU_MACHINE} -m 128M \
   -kernel ${KERNEL_IMAGE} -dtb ${DTB_IMAGE} -nographic
--- scripts/rpi3_64_glibc/firmware.sh
+++ scripts/rpi3_64_glibc/firmware.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+export WORK=${PROJECT_ROOT}/bootloader
+export DATA=${WORK}/data
+
+cd ${WORK}
+
 # Descargamos los archivos propietarios de la raspberry pi
 git clone --depth 1 https://github.com/raspberrypi/firmware
 
 # Copiamos los archivos al directorio data
-cp -iv firmware/boot/{bootcode.bin,fixup.dat,start.elf} data/
+cp -iv ${WORK}/firmware/boot/{bootcode.bin,fixup.dat,start.elf} ${DATA}
 
